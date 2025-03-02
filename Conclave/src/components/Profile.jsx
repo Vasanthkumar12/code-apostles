182c56e955babd8438dc829859ea7c01b60bd3b3
Vasanthkumar M <vasanth23402@gmail.com>
2025-03-02 18:10:44 +0530

    Done the functionalities of Register SignIn and some pages

--- Conclave/src/components/Profile.jsx
+++ Conclave/src/components/Profile.jsx
@@ -1,11 +1,26 @@
 import Navbar from "../common/Navbar"
+import { faUser } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../styles/Profile.css'
 
 const Profile = () => {
   return (
-    <>
+    <div className="profile">
      <Navbar/>
-     <div>Profile</div>
-    </>
+     <div id="pro_info">
+      <div id='pro-data'>
+        <div>
+          <p id="userIcon"><FontAwesomeIcon icon={faUser} id="user-icon" /></p>
+        </div>
+          <p><strong>Name : </strong>Peter Paul</p>
+          <p><strong>Email : </strong>peterpaul@gmail.com</p>
+          <p><strong>Phone : </strong>9344056723</p>
+          <p><strong>Interest : </strong>Religion</p>
+          <p><strong>Location : </strong>Vatican City, America</p>
+      </div>
+
+     </div>
+    </div>
    
   )
 }
